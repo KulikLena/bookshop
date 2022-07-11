96a5ed185fa276ccda0c2a63e43649ceeb7edebb
Lena Kulik <lenakulik6556@gmail.com>
2022-07-11 10:19:13 +0300

    comment

--- src/forecast/views.py
+++ src/forecast/views.py
@@ -11,11 +11,11 @@ def show_forecast_view(request):
     return HttpResponse (f"<h1>{forecast}, {username} </h1>")
 
 def show_mainpage_view(request):
-    print('In model Book will be placed unique instances of books in case some books could have several editions in different languages') 
+     
     out='<ul>'
     books=models.Book.objects.all()
     for bk in books: 
         out += f"<li> Book {bk.id} with native language book title {bk.native_language_book_title} was first published in {bk.first_published} </li>"
     out+='</ul>'
-    return HttpResponse(out)
+    return HttpResponse(out+'In model Book will be placed unique instances of books in case some books could have several editions in different languages')
    
